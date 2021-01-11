bea901393eabb6fb1d0e1359382b14a14da96312
IAFO-KumariAnamika <kumarianamika5798@gmail.com>
2021-01-11 14:52:00 +0530

    test

--- paper.js
+++ paper.js
@@ -3,23 +3,24 @@ class paper{
         var options={
             isStatic:false,
             restitution:0.3,
-            friction:0.5,
+            friction:0.3,
             density:1.2
 
         }
         this.x=x;
         this.y=y;
         this.r=r;
-        this.body=Bodies.circle(this.x,this.y,this.r/2,options);
+        this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options);
+        this.image=loadImage("paper.png")
         World.add(world,this.body);
     }
     display(){
         var pos=this.body.position;
         push();
         translate(pos.x,pos.y);
-        rectMode(CENTER);
+        imageMode(CENTER);
         strokeWeight(3);
         fill(255,0,255);
-        ellipse(0,0,this.r,this.r);
+        image(this.image,0,0,this.r,this.r);
     }
 }
